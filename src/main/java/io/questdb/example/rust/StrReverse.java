d85e71b659f49ffe5eae387a39ca8bef3ee1ae59
Libing Chen <libing.chen@gmail.com>
2024-03-24 14:40:03 +0800

    chore: polishing

--- src/main/java/io/questdb/example/rust/StrReverse.java
+++ src/main/java/io/questdb/example/rust/StrReverse.java
@@ -4,14 +4,8 @@ import io.questdb.jar.jni.JarJniLoader;
 
 public class StrReverse {
     static {
-           JarJniLoader.loadLib(
-                   Main.class,
-                   // A platform-specific path is automatically suffixed to path below.
-                   "/io/questdb/example/rust/libs",
+        JarJniLoader.loadLib(Main.class, "/io/questdb/example/rust/libs", "str_reverse");
+    }
 
-                   // The "lib" prefix and ".so|.dynlib|.dll" suffix are added automatically as needed.
-                   "str_reverse");
-       }
-
-       public static native String reversedString(String str);
+    public static native String reversedString(String str);
 }
