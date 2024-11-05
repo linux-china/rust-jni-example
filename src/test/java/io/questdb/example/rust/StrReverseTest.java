0a92e2d57d70a3868ddb6a6f13d6811fc1bbdbf4
Libing Chen <libing.chen@gmail.com>
2024-11-05 14:06:53 +0800

    test: upgrade to junit 5

--- src/test/java/io/questdb/example/rust/StrReverseTest.java
+++ src/test/java/io/questdb/example/rust/StrReverseTest.java
@@ -1,8 +1,9 @@
 package io.questdb.example.rust;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class StrReverseTest {
 
